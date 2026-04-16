b753ce44915e8e689ee3aacba60826e160458b53
baongoc1104 <ltbngoc.work@gmail.com>
2026-04-16 09:04:01 +0700

    refactor(recovery): remove obsolete SQL scripts and add flashback recovery test

--- Database/Subsystem2-MedicalDB/recovery/Create_Medical_Backup_Directory.sql
+++ Database/Subsystem2-MedicalDB/recovery/Create_Medical_Backup_Directory.sql
@@ -18,26 +18,3 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Created directory object MEDICAL_BACKUP_DIR and granted READ/WRITE to HOSPITAL_ADMIN.');
 END;
 /
-
-
--- SET SERVEROUTPUT ON;
--- DECLARE
---     l_info     SYS.KU$_DUMPFILE_INFO;
---     l_filetype NUMBER;
--- BEGIN
---     DBMS_DATAPUMP.GET_DUMPFILE_INFO(
---         filename   => 'MANUAL_LOGICAL_20260404164825.DMP',
---         directory  => 'MEDICAL_BACKUP_DIR',
---         info_table => l_info,
---         filetype   => l_filetype
---     );
-
---     DBMS_OUTPUT.PUT_LINE('OK: dump file is readable. filetype=' || l_filetype);
-
---     FOR i IN 1 .. l_info.COUNT LOOP
---         DBMS_OUTPUT.PUT_LINE(
---             'item_code=' || l_info(i).item_code || ', value=' || l_info(i).value
---         );
---     END LOOP;
--- END;
--- /
--- Database/Subsystem2-MedicalDB/recovery/test.sql
+++ Database/Subsystem2-MedicalDB/recovery/test.sql
@@ -1,18 +1,12 @@
-SELECT flashback_on FROM v$database;
-alter database flashback on;
 
--- Set the flash recovery area (if not already set)
-ALTER SYSTEM SET db_recovery_file_dest = 'D:/oracle-backup/fra';
+SHOW CON_NAME;
+SELECT STATUS FROM V$INSTANCE;
+SELECT OPEN_MODE FROM V$DATABASE;
+-- xóa 1 dòng trong bảng HSBA
+DELETE FROM hospital_admin.HSBA WHERE MAHSBA =1;
+commit;
 
--- Set the size for the flash recovery area
-ALTER SYSTEM SET db_recovery_file_dest_size = 10G;
-
--- Enable flashback
-SHUTDOWN IMMEDIATE;
-STARTUP MOUNT;
-ALTER DATABASE FLASHBACK ON;
-ALTER DATABASE OPEN;
-
+select * from hospital_admin.HSBA;
 
 -- dang nhap bang KTV
 select * from hospital_admin.HSBA ;
@@ -22,9 +16,3 @@ VALUES (4, TO_DATE('01-01-2026', 'DD-MM-YYYY'), 'TEST RECOVERY', 'Test', 'test',
 commit;
 
 select * from hospital_admin.HSBA where MABN = 4;
-
-ALTER TABLE hospital_admin.HSBA ENABLE ROW MOVEMENT;
--- Thực hiện flashback để khôi phục dữ liệu về trạng thái trước khi thực hiện INSERT
-FLASHBACK TABLE hospital_admin.HSBA TO TIMESTAMP TO_TIMESTAMP('2026-04-14 21:06:00', 'YYYY-MM-DD HH24:MI:SS');
-
-ALTER PLUGGABLE DATABASE XEPDB1 CLOSE IMMEDIATE;
